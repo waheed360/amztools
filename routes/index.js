ddaaf986166d1cc8d2dcbfb25e71a9b1175c74ca
Waheed <waheed360@gmail.com>
2019-05-09 17:25:15 -0400

    do not allow empty file upload

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const multer = require('multer');
 // setup storage
 const storage = multer.diskStorage({
     destination: './public/uploads/',
-    filename: function(req, file, cb) {
+    filename: (req, file, cb) => {
         cb(null,file.fieldname + '.' + Date.now() + path.extname(file.originalname))
     }
 });
@@ -19,23 +19,41 @@ const storage = multer.diskStorage({
 //setup upload
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 1}, //size in bytes
+    limits: { fileSize: 100000000000 }, //size in bytes
+    fileFilter: (req, file, cb) => {
+            checkFileType(file,cb);
+    }
 }).single('ufile')
 
+//check the checkFileType
+function checkFileType(file,cb){
+    //allowed extensiions
+    const filetypes = /csv/;
+    //check
+    const ext = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // check mimetype
+    const mimetype = filetypes.test(file.mimetype)
 
+    if (mimetype && ext){
+        return cb(null,true)
+    } else {
+        return cb ('Error: File must be .csv', false)
+    }
+}
+
+//TODO: this is broken
 router.post('/import', (req, res) => {
     upload(req, res, (err) => {
-        //TODO: this is broken
+        err = req.file ? null : 'Error: No File Uploaded';
         if(err) {
             res.render('index',{
-                message: err
+                message: 'Please select csv to upload',
+                error: err
             })
         } else {
-            console.log("lmao")
-            console.log(req.file)
+            return res.send('Received the request')
         }
     })
-    // return res.send('Received the request')
-
 });
+
 module.exports = router;
